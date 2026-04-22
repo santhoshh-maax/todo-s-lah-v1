23b3bd37b83e94255a640442575df076b9514b94
santhosh <santhoshpanneer03@gmail.com>
2026-04-22 13:38:18 +0530

    signature

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,16 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -10,11 +18,31 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.13599879"
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storePassword = keystoreProperties["storePassword"] as String?
+            
+            val stFile = keystoreProperties["storeFile"] as String?
+            if (stFile != null) {
+                storeFile = file(stFile)
+            }
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.santhosh.todoslah"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
     compileOptions {
-        
-        
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // This requires the desugar_jdk_libs dependency below
         isCoreLibraryDesugaringEnabled = true
     }
 
@@ -22,34 +50,17 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.santhosh.todoslah"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-        
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
-
-   buildTypes {
-    getByName("release") {
-        // Correct Kotlin syntax for signing config
-        signingConfig = signingConfigs.getByName("debug")
-        
-        // Use "=" for assignments in Kotlin
-        isMinifyEnabled = true
-        isShrinkResources = true
-        
-        // Use double quotes and parentheses for proguardFiles
-        proguardFiles(
-            getDefaultProguardFile("proguard-android-optimize.txt"),
-            "proguard-rules.pro"
-        )
-    }
-}
 }
 
 flutter {
@@ -57,13 +68,7 @@ flutter {
 }
 
 dependencies {
-    // Replace this line:
-    // implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
-    
-    // With this fixed line:
+    // This is what was missing or misplaced:
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
-
-    // Add this for desugaring support
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
-
